fc62d2573a887a65a6c44255a677c687682dd594
root <root@serverdev.cs.local>
2014-08-26 12:52:33 -0600

    Ruta bloqueada en mensaje

--- src/filters.php
+++ src/filters.php
@@ -1,13 +1,16 @@
 <?php
 
 Route::filter('cancerbero', function() {
+  $rolid = Config::get('cancerbero::rolidusuarios');
 
   if (Auth::guest()) return Redirect::guest(Config::get('cancerbero::rutalogin'));
-
-  $cancerbero = new Cancerbero;
-  $resultjson = $cancerbero->tienePermisos(Route::currentRouteName());
-  $result     = $resultjson->getData();
-
-  if(!$result->acceso)
-  	return View::make('cancerbero::error')->with('mensaje', $result->error);
+  //Si el rol es backdoor, ni busca los permisos, tiene permisos a todo.
+  if (Auth::user()->$rolid != Config::get('cancerbero::rolbackdoor')) {
+    $cancerbero = new Cancerbero;
+    $resultjson = $cancerbero->tienePermisos(Route::currentRouteName());
+    $result     = $resultjson->getData();
+  
+    if(!$result->acceso)
+      return View::make('cancerbero::error')->with('mensaje', $result->error . ' (' . Route::currentRouteName());
+  }
 });
