ddfdd00d5378955df416abb15b006947f7da86f1
root <root@serverdev.cs.local>
2014-09-08 15:38:35 -0600

    God mode

--- src/config/config.php
+++ src/config/config.php
@@ -43,7 +43,7 @@ return array(
 	
 	//'permisosocultos' => array('asignar', 'guardarpermisos', 'modulopermisos', 'guardarmodulopermisos'),
 	
-	'rolbackdoor'     => 2,
+	'rolbackdoor'     => 1,
 
 	'mensajemodulopermisoexitoso' => 'Se han actualizado los permisos para los m&oacute;dulos exitosamente.',
 
