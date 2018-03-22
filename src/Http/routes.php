e54d596e0a6f3b376e5cfd8b8599a543dc22da3c
Jose Galindo <jgalindo@cs.com.gt>
2018-03-21 18:10:19 -0600

    Cancerbero routes adios

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,11 +1,1 @@
 <?php
-
-	$routedata = ['namespace' => 'Csgt\Cancerbero\Http\Controllers', 'middleware'=>['auth']];
-	if(config('csgtcancerbero.routeextras')) {
-		$routedata = array_merge($routedata, config('csgtlogin.routeextras',[]));
-	}
-
-	Route::group($routedata, function() {
-		Route::get ('cancerbero/asignar/{id}', 'cancerberoController@show');
-		Route::post('cancerbero/asignar', 'cancerberoController@store');
-	});
