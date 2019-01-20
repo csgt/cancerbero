c9a7b1018416e84c13f47bc1ac96be4f087e641c
Jose Galindo <jgalindo@softlogic.com.gt>
2019-01-20 07:42:41 -0600

    Fix a migration - add parent_id

--- src/database/migrations/2014_12_15_091721_migration_create_permissions_table.php
+++ src/database/migrations/2014_12_15_091721_migration_create_permissions_table.php
@@ -9,8 +9,11 @@ class MigrationCreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('parent_id');
             $table->string('name', 50);
             $table->string('description', 50);
+
+            $table->foreign('parent_id')->references('id')->on('permissions')->onUpate('cascade');
         });
     }
 
