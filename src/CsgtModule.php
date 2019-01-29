bec924b70818297a1af93fee3ce2c48a5c4ce825
Jose Galindo <jgalindo@softlogic.com.gt>
2019-01-29 16:49:45 -0600

    Add extra parameter to store menupermission

--- src/CsgtModule.php
+++ src/CsgtModule.php
@@ -7,22 +7,24 @@ class CsgtModule
     const INDEX  = [1];
     const ALL    = [1, 2, 3, 4, 5, 6, 7, 8];
 
-    public $name         = null;
-    public $description  = null;
-    public $module       = null;
-    public $menuOrder    = 0;
-    public $permissions  = self::ALL;
-    public $icon         = null;
-    public $parentModule = null;
+    public $name           = null;
+    public $description    = null;
+    public $module         = null;
+    public $menuOrder      = 0;
+    public $permissions    = self::ALL;
+    public $icon           = null;
+    public $parentModule   = null;
+    public $menuPermission = 1;
 
-    public function __construct($aName, $aDescription, $aModule, $aMenuOrder, $aIcon = null, $aParentModule = null, $aPermissions = self::ALL)
+    public function __construct($aName, $aDescription, $aModule, $aMenuOrder, $aIcon = null, $aParentModule = null, $aPermissions = self::ALL, $aMenuPermission = 1)
     {
-        $this->name         = $aName;
-        $this->description  = $aDescription;
-        $this->module       = $aModule;
-        $this->menuOrder    = $aMenuOrder;
-        $this->permissions  = $aPermissions;
-        $this->icon         = $aIcon;
-        $this->parentModule = $aParentModule;
+        $this->name           = $aName;
+        $this->description    = $aDescription;
+        $this->module         = $aModule;
+        $this->menuOrder      = $aMenuOrder;
+        $this->permissions    = $aPermissions;
+        $this->icon           = $aIcon;
+        $this->parentModule   = $aParentModule;
+        $this->menuPermission = $aMenuPermission;
     }
 }
