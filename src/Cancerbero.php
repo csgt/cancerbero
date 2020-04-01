8343a0ec124e2f0140897d553617cdefb267a51c
Jose Galindo <jgalindo@softlogic.com.gt>
2020-04-01 12:07:47 -0600

    funcion can()

--- src/Cancerbero.php
+++ src/Cancerbero.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Csgt\Cancerbero;
 
 use Auth;
@@ -9,15 +8,22 @@ use App\Models\Cancerbero\Authmodulopermiso;
 
 class Cancerbero
 {
-    public static function tienePermisos($aRuta, $aRedirect=true, $aSimple=false)
+    public static function can($aRuta)
+    {
+        return self::tienePermisos($aRuta, false, true);
+
+    }
+
+    public static function tienePermisos($aRuta, $aRedirect = true, $aSimple = false)
     {
         // dd(Auth::guest(), $aRedirect);
         if (Auth::guest()) {
-            if ($aRedirect===true) {
+            if ($aRedirect === true) {
                 return redirect()->guest(config('csgtcancerbero.rutalogin'));
             } else {
                 $response['error']  = 'Usuario no autenticado';
                 $response['acceso'] = false;
+
                 return response()->json($response);
             }
         }
@@ -27,70 +33,74 @@ class Cancerbero
         $permiso = 'index';
 
         if (count($arr) >= 2) {
-            $permiso = $arr[count($arr)-1];
+            $permiso = $arr[count($arr) - 1];
             array_pop($arr);
-            $modulo  = implode('.', $arr);
+            $modulo = implode('.', $arr);
         } elseif (count($arr == 1)) {
-            $modulo  = $arr[0];
+            $modulo = $arr[0];
         }
 
         if ($modulo == '') {
-            if ($aSimple===true) {
+            if ($aSimple === true) {
                 return false;
             }
             $response['error']  = config('csgtcancerbero.errorenrutas');
             $response['acceso'] = false;
+
             return response()->json($response);
         }
 
-        $authModulo  = Authmodulo::where('nombre', $modulo)->first();
+        $authModulo = Authmodulo::where('nombre', $modulo)->first();
         if ($authModulo == null) {
-            if ($aSimple===true) {
+            if ($aSimple === true) {
                 return false;
             }
             $response['error']  = 'No existe ese modulo.';
             $response['acceso'] = false;
+
             return response()->json($response);
         }
         $moduloid = $authModulo->moduloid;
 
         $authPermiso = Authpermiso::where('nombre', $permiso)->first();
         if ($authPermiso == null) {
-            if ($aSimple===true) {
+            if ($aSimple === true) {
                 return false;
             }
             $response['error']  = 'No existe ese permiso.';
             $response['acceso'] = false;
+
             return response()->json($response);
         }
         $permisoid = $authPermiso->permisoid;
 
-
         $usuarioroles = Auth::user()->getRoles();
 
         $ds = Authmodulopermiso::with([
-            'authrolmodulopermisos'=>function ($query) use ($usuarioroles) {
+            'authrolmodulopermisos' => function ($query) use ($usuarioroles) {
                 $query->whereIn('authrolmodulopermisos.rolid', $usuarioroles);
-            }
+            },
         ])->where('moduloid', $moduloid)
-        ->where('permisoid', $permisoid)->first();
+            ->where('permisoid', $permisoid)->first();
 
         if ($ds) {
             $cuantos = $ds->authrolmodulopermisos->count();
-            if ($cuantos>0) {
-                if ($aSimple===true) {
+            if ($cuantos > 0) {
+                if ($aSimple === true) {
                     return true;
                 }
                 $response['error']  = '';
                 $response['acceso'] = true;
+
                 return response()->json($response);
             }
         }
-        if ($aSimple===true) {
+        if ($aSimple === true) {
             return false;
         }
         $response['error']  = trans('cancerbero.accesodenegado');
         $response['acceso'] = false;
+
         return response()->json($response);
     }
 
@@ -100,27 +110,28 @@ class Cancerbero
         $edit   = false;
         $delete = false;
 
-        $addjson = self::tienePermisos($aModulo.'.create', false);
+        $addjson = self::tienePermisos($aModulo . '.create', false);
         if ($addjson) {
-            $add     = $addjson->getData()->acceso;
+            $add = $addjson->getData()->acceso;
         }
 
-        $editjson = self::tienePermisos($aModulo.'.edit', false);
+        $editjson = self::tienePermisos($aModulo . '.edit', false);
         if ($editjson) {
-            $edit     = $editjson->getData()->acceso;
+            $edit = $editjson->getData()->acceso;
         }
 
-        $deletejson = self::tienePermisos($aModulo.'.destroy', false);
+        $deletejson = self::tienePermisos($aModulo . '.destroy', false);
         if ($deletejson) {
-            $delete     = $deletejson->getData()->acceso;
+            $delete = $deletejson->getData()->acceso;
         }
-        return ['add'=>$add, 'edit'=>$edit, 'delete'=>$delete];
+
+        return ['add' => $add, 'edit' => $edit, 'delete' => $delete];
     }
 
     public static function isGod()
     {
         if (Auth::check()) {
-            $rolbackdoor = config('csgtcancerbero.rolbackdoor');
+            $rolbackdoor  = config('csgtcancerbero.rolbackdoor');
             $usuarioroles = Auth::user()->getRoles();
 
             return (in_array($rolbackdoor, $usuarioroles));
