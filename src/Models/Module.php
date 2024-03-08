26f2da9333cb3d25a147ce13769b9cc7ed91dc29
Jose Galindo Mini <66330140+jgalindosl@users.noreply.github.com>
2024-03-07 18:20:45 -0600

    missing model relations

--- src/Models/Module.php
+++ src/Models/Module.php
@@ -8,4 +8,14 @@ class Module extends Model
     public $timestamps    = false;
     protected $primaryKey = 'name';
     public $incrementing  = false;
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permissions::class);
+    }
+
+    public function modulepermissions()
+    {
+        return $this->hasMany(ModulePermission::class);
+    }
 }
